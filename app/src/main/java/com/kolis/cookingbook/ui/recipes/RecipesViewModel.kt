0bdc7e5e08276eae38226317110039b873e01e1b
Aleksey Kolisnichenko <aleksey.kolisnichenko@dstarlab.com>
2020-09-18 11:18:59 +0300

    UI fixes of basic android design

--- app/src/main/java/com/kolis/cookingbook/ui/recipes/RecipesViewModel.kt
+++ app/src/main/java/com/kolis/cookingbook/ui/recipes/RecipesViewModel.kt
@@ -1,10 +1,10 @@
-package com.kolis.cookingbook.ui.home
+package com.kolis.cookingbook.ui.recipes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class RecipesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
